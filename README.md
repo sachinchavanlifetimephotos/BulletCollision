cc6b3af1703dec4cacc6454c6ed8b6de115d05ff
sachinchavanlifetimephotos <75015277+sachinchavanlifetimephotos@users.noreply.github.com>
2021-01-19 14:57:50 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# BulletCollision
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,44 @@
+var bullet, wall; 
+var speed, weight; 
+var thickness; 
+
+
+function setup(){  
+createCanvas(1600,400)
+
+speed=random(223,321)
+weight=random(30,52) 
+
+bullet=createSprite(50,250,20,20); 
+bullet.velocityX=speed; 
+bullet.shapeColor=color(0); 
+
+thickness=random(22,83); 
+
+wall=createSprite(1200,200,thickness,height/2);  
+wall=shapeColor=color(80,80,80); 
+
+} 
+
+function draw(){  
+background("yellow"); 
+
+if(wall.x-bullet.x <(bullet.width+wall.width)/2){  
+bullet.velocityX=0; 
+var deformation=0.5*   weight * speed * speed/22509; 
+if(deformation>180){  
+bullet.shapeColor=color(255,0,0); 
+}
+
+if(deformation<180 && deformation>100){  
+
+bullet.shapeColor=color(230,230,3); 
+}
+
+if(deformation<100){  
+bullet.shapeColor=color(0,255,0); 
+}  
+
+} 
+drawSprites(); 
+} 
